9920ecd61f16ea917903af60f3d919f82305d6f7
vladyslav shalamai <vladyslavsh@wix.com>
2023-12-11 16:28:24 +0200

    day 7 part 2

--- src/day7/Solution2.java
+++ src/day7/Solution2.java
@@ -6,15 +6,15 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Solution {
+public class Solution2 {
 
     public static void main(String[] args) throws IOException {
-        var res1 = part1("./src/day7/input.txt");
+        var res1 = part2("./src/day7/input.txt");
         System.out.println(res1);
     }
 
 
-    static int part1(String path) throws IOException {
+    static int part2(String path) throws IOException {
         var hands = input(path);
         hands.sort(handsComparator);
         int acc = 0;
@@ -26,17 +26,17 @@ public class Solution {
         return acc;
     }
 
-    static List<Hand> input(String path) throws IOException {
+    static List<Hand2> input(String path) throws IOException {
         return Files.readAllLines(Path.of(path))
                 .stream()
                 .map(l -> {
                     var parts = l.split(" ");
-                    return new Hand(parts[0], Integer.parseInt(parts[1]));
+                    return new Hand2(parts[0], Integer.parseInt(parts[1]));
                 })
                 .collect(Collectors.toList());
     }
 
-    static Comparator<Hand> handsComparator = (a, b) -> {
+    static Comparator<Hand2> handsComparator = (a, b) -> {
         if (a.type != b.type) return a.type - b.type;
         else for (int i = 0; i < a.cards.size(); i++) {
             if (!Objects.equals(a.cards.get(i), b.cards.get(i))) return a.cards.get(i) - b.cards.get(i);
@@ -46,7 +46,7 @@ public class Solution {
     };
 }
 
-class Hand {
+class Hand2 {
     List<Integer> cards;
     int bid;
 
@@ -54,7 +54,7 @@ class Hand {
 
     private Map<Character, Integer> code2int = Map.of(
             'T', 10,
-            'J', 11,
+            'J', 1,
             'Q', 12,
             'K', 13,
             'A', 14
@@ -62,7 +62,7 @@ class Hand {
 
     private List<String> combPrefixes = List.of("5", "4", "32", "3", "22", "2", "1");
 
-    Hand(String cards, int bid) {
+    Hand2(String cards, int bid) {
         this.cards = cards
                 .chars()
                 .map(c -> Character.isDigit(c)
@@ -77,16 +77,28 @@ class Hand {
 
     void determineType() {
         var combination = this.cards.stream()
+                .filter(c -> c != 1)
                 .collect(Collectors.groupingBy(c -> c))
                 .values()
                 .stream()
                 .map(List::size)
                 .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
+
+        var jokers = (int) this.cards.stream().filter(c -> c == 1).count();
+        if (jokers == 5) {
+            combination.add(5);
+        } else {
+            combination.set(0, combination.get(0) + jokers);
+        }
+
+
+        var combination2 = combination.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining());
 
         for (int i = 0; i < combPrefixes.size(); i++) {
-            if (combination.startsWith(combPrefixes.get(i))) {
+            if (combination2.startsWith(combPrefixes.get(i))) {
                 this.type = combPrefixes.size() - i;
                 return;
             }
