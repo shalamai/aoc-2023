3e82e9f3d24c1ca7ddd7de5eb3070eda2a5dc236
vladyslav shalamai <vladyslavsh@wix.com>
2023-12-12 00:02:44 +0200

    day 8 part 2

--- src/day8/Solution.java
+++ src/day8/Solution.java
@@ -11,6 +11,9 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         int res1 = part1("./src/day8/input.txt");
         System.out.println(res1);
+
+        long res2 = part2("./src/day8/input.txt");
+        System.out.println(res2);
     }
 
     static int part1(String path) throws IOException {
@@ -33,6 +36,18 @@ public class Solution {
         return steps;
     }
 
+    static long part2(String path) throws IOException {
+        var instructions = instructions(path);
+        var map = map(path);
+
+        return map.keySet().stream()
+                .filter(n -> n.endsWith("A"))
+                .map(n -> stepsToZ(n, instructions, map))
+                .reduce(Solution::lcm)
+                .get();
+    }
+
+
     static String instructions(String path) throws IOException {
         return Files.readAllLines(Path.of(path)).get(0);
     }
@@ -44,5 +59,37 @@ public class Solution {
                 .collect(Collectors.toMap(l -> l.substring(0, 3), l -> new String[]{
                         l.substring(7, 10), l.substring(12, 15)
                 }));
+    }
+
+    static long stepsToZ(String node, String instructions, Map<String, String[]> map) {
+        var steps = 0L;
+        var instrIdx = 0;
+
+        while (!node.endsWith("Z")) {
+            steps++;
+            node = instructions.charAt(instrIdx) == 'L'
+                    ? map.get(node)[0]
+                    : map.get(node)[1];
+            instrIdx = instrIdx < instructions.length() - 1
+                    ? instrIdx + 1
+                    : 0;
+        }
+
+        return steps;
+    }
+
+    static long lcm(long number1, long number2) {
+        if (number1 == 0 || number2 == 0) {
+            return 0;
+        }
+        var absNumber1 = Math.abs(number1);
+        var absNumber2 = Math.abs(number2);
+        var absHigherNumber = Math.max(absNumber1, absNumber2);
+        var absLowerNumber = Math.min(absNumber1, absNumber2);
+        var lcm = absHigherNumber;
+        while (lcm % absLowerNumber != 0) {
+            lcm += absHigherNumber;
+        }
+        return lcm;
     }
 }
