608730599d325ccb10ad57ce720781777271f1c3
vladyslav shalamai <vladyslavsh@wix.com>
2023-12-18 12:39:44 +0200

    day 17 part 1 wip 6

--- src/day17/Solution.java
+++ src/day17/Solution.java
@@ -31,33 +31,34 @@ public class Solution {
     static int part1(String path) throws IOException {
         var grid = input(path);
 
-        var costs = new ArrayList<List<Map<Direction, Integer>>>();
-        for (int i = 0; i < grid.length; i++) {
-            costs.add(new ArrayList<>());
-            for (int j = 0; j < grid[0].length; j++) {
-                costs.get(i).add(new HashMap());
-            }
-        }
+        var costs = new HashMap<Coord, Map<Direction, Integer>>();
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                costs.put(new Coord(i, j), new HashMap());
 
         Queue<Cell> q = new LinkedList<>();
 
-        q.add(new Cell(0, 0, new Direction(FROM.left, 0)));
-        costs.get(0).get(0).put(new Direction(FROM.left, 0), 0);
+        var init = new Cell(new Coord(0, 0), new Direction(FROM.left, 0));
+        q.add(init);
+        costs.get(init.coord).put(init.dir, 0);
 
         while (!q.isEmpty()) {
             var cell = q.poll();
 
             for (Cell next : cell.neighbours(grid.length - 1, grid[0].length - 1)) {
-                var newCost = costs.get(cell.i).get(cell.j).get(cell.dir) + grid[next.i][next.j];
+                var newCost = costs.get(cell.coord).get(cell.dir) + grid[next.coord.i][next.coord.j];
 
-                if (costs.get(next.i).get(next.j).entrySet().stream().filter(c -> c.getKey().from == next.dir.from && c.getKey().consecutive <= next.dir.consecutive && c.getValue() <= newCost).findFirst().isEmpty()) {
-                    costs.get(next.i).get(next.j).put(next.dir, newCost);
+                var betterCostExists = costs.get(next.coord).entrySet().stream()
+                        .anyMatch(c -> c.getKey().from == next.dir.from && c.getKey().consecutive <= next.dir.consecutive && c.getValue() <= newCost);
+
+                if (!betterCostExists) {
+                    costs.get(next.coord).put(next.dir, newCost);
                     q.add(next);
                 }
             }
         }
 
-        return costs.get(costs.size() - 1).get(costs.get(0).size() - 1).values().stream().min(Integer::compareTo).get();
+        return costs.get(new Coord(grid.length - 1, grid[0].length - 1)).values().stream().min(Integer::compareTo).get();
     }
 
     static int[][] input(String path) throws IOException {
@@ -67,7 +68,7 @@ public class Solution {
                 .toArray(int[][]::new);
     }
 
-    record Cell(int i, int j, Direction dir) {
+    record Cell(Coord coord, Direction dir) {
         List<Cell> neighbours(int maxI, int maxJ) {
             return Stream.of(up(), down(maxI), left(), right(maxJ))
                     .filter(Optional::isPresent)
@@ -76,32 +77,35 @@ public class Solution {
         }
 
         Optional<Cell> up() {
-            if (i == 0 || dir.from == FROM.top || (dir.from == FROM.bottom && dir.consecutive == 3))
+            if (coord.i == 0 || dir.from == FROM.top || (dir.from == FROM.bottom && dir.consecutive == 3))
                 return Optional.empty();
             else
-                return Optional.of(new Cell(i - 1, j, new Direction(FROM.bottom, dir.from == FROM.bottom ? dir.consecutive + 1 : 1)));
+                return Optional.of(new Cell(new Coord(coord.i - 1, coord.j), new Direction(FROM.bottom, dir.from == FROM.bottom ? dir.consecutive + 1 : 1)));
         }
 
         Optional<Cell> down(int maxI) {
-            if (i == maxI || dir.from == FROM.bottom || (dir.from == FROM.top && dir.consecutive == 3))
+            if (coord.i == maxI || dir.from == FROM.bottom || (dir.from == FROM.top && dir.consecutive == 3))
                 return Optional.empty();
             else
-                return Optional.of(new Cell(i + 1, j, new Direction(FROM.top, dir.from == FROM.top ? dir.consecutive + 1 : 1)));
+                return Optional.of(new Cell(new Coord(coord.i + 1, coord.j), new Direction(FROM.top, dir.from == FROM.top ? dir.consecutive + 1 : 1)));
         }
 
         Optional<Cell> left() {
-            if (j == 0 || dir.from == FROM.left || (dir.from == FROM.right && dir.consecutive == 3))
+            if (coord.j == 0 || dir.from == FROM.left || (dir.from == FROM.right && dir.consecutive == 3))
                 return Optional.empty();
             else
-                return Optional.of(new Cell(i, j - 1, new Direction(FROM.right, dir.from == FROM.right ? dir.consecutive + 1 : 1)));
+                return Optional.of(new Cell(new Coord(coord.i, coord.j - 1), new Direction(FROM.right, dir.from == FROM.right ? dir.consecutive + 1 : 1)));
         }
 
         Optional<Cell> right(int maxJ) {
-            if (j == maxJ || dir.from == FROM.right || (dir.from == FROM.left && dir.consecutive == 3))
+            if (coord.j == maxJ || dir.from == FROM.right || (dir.from == FROM.left && dir.consecutive == 3))
                 return Optional.empty();
             else
-                return Optional.of(new Cell(i, j + 1, new Direction(FROM.left, dir.from == FROM.left ? dir.consecutive + 1 : 1)));
+                return Optional.of(new Cell(new Coord(coord.i, coord.j + 1), new Direction(FROM.left, dir.from == FROM.left ? dir.consecutive + 1 : 1)));
         }
+    }
+
+    record Coord(int i, int j) {
     }
 
     record Direction(FROM from, int consecutive) {
