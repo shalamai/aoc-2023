e519453a3f6532a398d786faf505f9cb27b21c28
vladyslav shalamai <vladyslavsh@wix.com>
2023-12-18 11:25:05 +0200

    day 17 part 1 wip 2

--- src/day17/Solution.java
+++ src/day17/Solution.java
@@ -6,43 +6,60 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Stream;
 
+// refactor this shit
+// skip if there is a cost with less value and less-or-equal conseq
+// try to improve performance
+// - before refactor: 3723529 ops, 9527 ms
+// - after: 2110969 ops, 4759 ms
+// use Cell(with val) instead of Cost
+// hide costs array inside data structure. Cell has all fields needed to work with it
+// rm trail
+
+// code 2 part
+// - adjust cell.next(min 4, max 10)
+// - res filter (conseq >= 4)
+
 public class Solution {
 
     public static void main(String[] args) throws IOException {
+        var start = System.currentTimeMillis();
         var res1 = part1("./src/day17/input.txt");
+        System.out.println("done in " + (System.currentTimeMillis() - start));
         System.out.println(res1);
     }
 
-    // todo: starting position = 0
     static int part1(String path) throws IOException {
         var grid = input(path);
-        var costs = new int[grid.length][grid[0].length];
-        var visited = new boolean[grid.length][grid[0].length];
-        var trails = new Cell[grid.length][grid[0].length];
+        var costs = new ArrayList<List<Map<Integer, Cost>>>();
+
+        for (int i = 0; i < grid.length; i++) {
+            costs.add(new ArrayList<>());
+            for (int j = 0; j < grid[0].length; j++) {
+                costs.get(i).add(new HashMap());
+            }
+        }
+
         Queue<Cell> q = new LinkedList<>();
 
         q.add(new Cell(0, 0, FROM.left, 0, new ArrayList<>()));
-        visited[0][0] = true;
+        var init = new Cost(FROM.left, 0, 0);
+        costs.get(0).get(0).put(init.hashCode(), init);
 
         while (!q.isEmpty()) {
             var cell = q.poll();
 
             for (Cell next : cell.neighbours(grid.length - 1, grid[0].length - 1)) {
-                var newCost = costs[cell.i][cell.j] + grid[next.i][next.j];
+                var newCost = costs.get(cell.i).get(cell.j).get(new Cost(cell.from, cell.consecutive, 0).hashCode()).val + grid[next.i][next.j];
 
-                if (!visited[next.i][next.j] || costs[next.i][next.j] > newCost) {
-                    costs[next.i][next.j] = newCost;
-                    visited[next.i][next.j] = true;
-                    trails[next.i][next.j] = next;
+                if (costs.get(next.i).get(next.j).values().stream().filter(c -> c.from == next.from && c.consecutive <= next.consecutive && c.val <= newCost).findFirst().isEmpty()) {
+                    var nc = new Cost(next.from, next.consecutive, newCost);
+                    costs.get(next.i).get(next.j).put(nc.hashCode(), nc);
                     q.add(next);
                 }
             }
         }
 
-//        Arrays.stream(costs).forEach(a -> System.out.println(Arrays.toString(a)));
-//        trails[costs.length - 1][costs[0].length - 1].trail.forEach(System.out::println);
-
-        return costs[costs.length - 1][costs[0].length - 1];
+        return costs.get(costs.size() - 1).get(costs.get(0).size() - 1).values().stream().map(c -> c.val).min(Integer::compareTo).get();
     }
 
     static int[][] input(String path) throws IOException {
@@ -104,6 +121,22 @@ public class Solution {
                     ", from=" + from +
                     ", consecutive=" + consecutive +
                     '}';
+        }
+    }
+
+    record Cost(FROM from, int consecutive, int val) {
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Cost cost = (Cost) o;
+            return consecutive == cost.consecutive && from == cost.from;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(from, consecutive);
         }
     }
 
