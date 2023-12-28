aab7e50aa3c1d9dcef9105dbd9a79c5ddc80ba7b
vladyslav shalamai <vladyslavsh@wix.com>
2023-12-28 21:08:35 +0200

    day 23 part 2

--- src/day23/Solution.java
+++ src/day23/Solution.java
@@ -8,6 +8,9 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         var res1 = part1("./src/day23/input.txt");
         System.out.println(res1);
+
+        var res2 = part2("./src/day23/input.txt");
+        System.out.println(res2);
     }
 
     static int part1(String path) throws IOException {
@@ -36,13 +39,67 @@ public class Solution {
                     .filter(n -> n.i > 0 && n.i < grid.length && n.j > 0 && n.j < grid[0].length &&
                             (visited.get(n.i).get(n.j).stream().noneMatch(n.pathId::startsWith)) &&
                             (grid[n.i][n.j] == '.' || (grid[n.i][n.j] == n.dir)))
-                    .map(n -> n.appendToPath(randomString()))
                     .toList();
+
+            next = next.size() > 1 ? next.stream().map(n -> n.appendToPath(randomString())).toList() : next;
 
             q.addAll(next);
         }
 
         return max;
+    }
+
+    static int part2(String path) throws IOException {
+        var grid = input(path);
+        var graph = buildGraph(grid);
+        var finish = new Coord(grid.length - 1, grid[0].length - 2);
+
+        var max = 0;
+        Queue<Answer> q = new LinkedList<>();
+        q.add(new Answer(List.of(new Coord(0, 1)), 0));
+
+        while (!q.isEmpty()) {
+            var curr = q.poll();
+            var last = curr.path.get(curr.path.size() - 1);
+
+            if (last.equals(finish)) max = Math.max(curr.steps, max);
+            else q.addAll(graph.get(last).entrySet().stream()
+                    .filter(e -> !curr.path.contains(e.getKey()))
+                    .map(curr::advance)
+                    .toList());
+        }
+
+        return max;
+    }
+
+    static Map<Coord, Map<Coord, Integer>> buildGraph(char[][] grid) {
+        var res = new HashMap<Coord, Map<Coord, Integer>>();
+
+        Queue<Edge> q = new LinkedList<>();
+        q.add(new Edge(new Coord(0, 1), new Coord(0, 1), new Coord(0, 1), 0));
+        var visited = new ArrayList<Coord>();
+
+        while (!q.isEmpty()) {
+            var e = q.poll();
+
+            var next = e.curr.next().stream()
+                    .filter(n -> n.i > 0 && n.i < grid.length && n.j > 0 && n.j < grid[0].length
+                            && (grid[n.i][n.j] != '#') && !e.prev.equals(n))
+                    .toList();
+
+            if (next.size() == 1) q.add(new Edge(e.from, e.curr, next.get(0), e.steps + 1));
+            else if (next.size() > 1 || e.curr.equals(new Coord(grid.length - 1, grid[0].length - 2))) {
+                q.addAll(next.stream()
+                        .filter(n -> !visited.contains(n))
+                        .map(n -> new Edge(e.curr, e.curr, n, 1))
+                        .toList());
+                visited.addAll(next);
+                if (!res.containsKey(e.from)) res.put(e.from, new HashMap<>());
+                res.get(e.from).put(e.curr, e.steps);
+            }
+        }
+
+        return res;
     }
 
     static char[][] input(String path) throws IOException {
@@ -62,6 +119,28 @@ public class Solution {
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
+    }
+
+    record Edge(Coord from, Coord prev, Coord curr, int steps) {
+    }
+
+    record Coord(int i, int j) {
+        List<Coord> next() {
+            return List.of(
+                    new Coord(i + 1, j),
+                    new Coord(i - 1, j),
+                    new Coord(i, j + 1),
+                    new Coord(i, j - 1)
+            );
+        }
+    }
+
+    record Answer(List<Coord> path, int steps) {
+        Answer advance(Map.Entry<Coord, Integer> next) {
+            var path2 = new ArrayList<>(path);
+            path2.add(next.getKey());
+            return new Answer(path2, this.steps + next.getValue());
+        }
     }
 
     record Path(String pathId, int i, int j, int steps, char dir) {
